8c726cd3fb84491318e5e9763307a960029a8fea
WinterSeasonn <jasonha567@gmail.com>
2025-05-15 18:26:52 -0400

    eye_detection

--- face_detection/face_capture.py
+++ face_detection/face_capture.py
@@ -1,10 +1,12 @@
 from detector import Detector
 import cv2 as cv
+import dlib
 
 # Initializing detector
 detector = Detector(
     prototxt_path="face_detection/models/deploy.prototxt", 
-    model_path="face_detection/models/res10_300x300_ssd_iter_140000.caffemodel"
+    model_path="face_detection/models/res10_300x300_ssd_iter_140000.caffemodel",
+    predictor_path="face_detection/models/shape_predictor_68_face_landmarks.dat"
 )
 
 # Initializing camera
@@ -13,6 +15,9 @@ if not camera.isOpened:
     print("Camera failed to open.")
     exit()
 
+
+dlib = dlib.shape_predictor("")
+
 # Using camera input to detect faces
 while True:
     ret, frame = camera.read()
@@ -20,11 +25,15 @@ while True:
         break
 
     # Detecting faces in frame
-    faces = detector.detect(frame)
+    faces, eyes = detector.detect(frame)
     # Drawing bounding box
     for(x, y, w, h) in faces:
         cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    cv.imshow("Face Detection", frame)
+
+    for(le,re) in eyes:
+        for(ex,ey) in le + re:
+            cv.circle(frame, (ex,ey),2, (255,0,0), -1)
+  
 
     # Checks if ESC key is pressed, and breaks if so
     if cv.waitKey(1) & 0xFF == 27:
