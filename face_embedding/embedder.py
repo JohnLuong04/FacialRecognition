ed9fc11a0d9001097c8331dd0a48ab36243a7df7
John Luong <scorpionf904@gmail.com>
2025-05-18 20:42:26 -0400

    implemented face embedding

--- face_embedding/embedder.py
+++ face_embedding/embedder.py
@@ -1,3 +1,22 @@
-"""
-This is supposed to take an aligned face image and embed it into a vector
-"""
+from deepface import DeepFace
+import cv2 as cv
+import numpy as np
+import os
+
+input_folder = "database/initial_face_images"
+output_folder = "database/embedded_face_vectors"
+os.makedirs(output_folder, exist_ok=True)
+
+for image in os.listdir(input_folder):
+    image_path = os.path.join(input_folder, image)
+    img = cv.imread(image_path)
+
+    if img is None:
+        print(f"Failed to load {image_path}")
+        continue
+    try:
+        embedding = DeepFace.represent(img_path=image_path, model_name='Facenet', enforce_detection=False)[0]["embedding"]
+        name = os.path.splitext(image)[0]
+        np.save(os.path.join(output_folder, f"{name}.npy"), embedding)
+    except Exception as e:
+        print(f"Error processing {image}: {e}")
